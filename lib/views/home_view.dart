acade098bcd197f112c91b019302b3faa20435df
Ahmed Abdelgawad <hagahm721@gmail.com>
2025-09-20 15:01:22 +0300

    add Banner auto play

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,63 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
+  final List<String> banners = [
+    "assets/banners/Slider 1.png",
+    "assets/banners/Slider 2.png",
+    "assets/banners/Slider 3.png",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: const Icon(Icons.motorcycle, size: 25),
+          title: Row(
+            children: [
+              const Text(
+                "61 Hopper Street..",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.keyboard_arrow_down_outlined, size: 25),
+              Spacer(),
+              SvgPicture.asset('assets/icons/basket.svg'),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                autoPlayCurve: Curves.linear,
+                height: 220,
+                autoPlayAnimationDuration: Duration(seconds: 3),
+                autoPlayInterval: Duration(seconds: 3),
+                enlargeCenterPage: true,
+                viewportFraction: 0.6,
+
+              ),
+
+              items: banners.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Image(image: AssetImage(i)),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -13,8 +13,8 @@ class _SplashViewState extends State<SplashView> {
 @override
 void initState() {
   super.initState();
-  Future.delayed(const Duration(seconds: 3), () {
-    Navigator.push(context, MaterialPageRoute(builder: (context)=> const HomeView()));
+  Future.delayed(const Duration(seconds: 2), () {
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>  HomeView()));
   });
 }
 
